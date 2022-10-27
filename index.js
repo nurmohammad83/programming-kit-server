4823a99d052e0b898ccff49781fe49b7f0ac41cb
nurmohammad83 <pesfootball83@gmail.com>
2022-10-27 06:49:13 +0600

    versel i

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ app.use(cors());
 
 const port = process.env.Port || 5000;
 
-app.get('/courses-name', (req, res)=>{
-    res.send(courseName)
-})
-
-
-
-
-
 
 app.get('/courses', (req, res)=>{
     res.send(courses)
 })
 
 
+
+app.get('/courses-name/:id', (req, res)=>{
+    const id = req.params.id;
+    const coursesName = courses.find(c=> c.courses_id == id)
+    res.send(coursesName)
+    console.log(id);
+})
 app.get('/courses/:id', (req, res)=>{
     const id = req.params.id;
     const coursesDetails = courses.find(coures=> coures._id=== id)
     res.send(coursesDetails)
+    console.log(id);
 })
 
 app.listen(port, ()=>{
